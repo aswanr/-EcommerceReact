7ccae5a32e588fde11386e4f2ce1dfa86f2b762c
Arjun <arjun.k@coddletech.com>
2025-01-27 18:12:19 +0530

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Report Web Vitals to console (or send to an analytics endpoint)
+reportWebVitals(console.log);
+
+
